0acd2e7fee39f90aac4c3d39f661720b203afe91
Patrik Jonell <pjjonell@kth.se>
2017-07-13 15:33:31 +0100

    moved artificial_agent to game

--- src/main/python/game/agent/agent.py
+++ src/main/python/game/agent/agent.py
@@ -1,25 +1,32 @@
 # import fatima
 from furhat import connect_to_iristk
+import pika
+# from .. import Player
+from threading import Thread
+import json
 # etc..
 
 
-class ArtificialAgent(Participant):
-    """docstring for ArtificialAgent."""
+class Agent(object):
+    """docstring for Agent."""
 
-    FURHAT_IP = 'x.x.x.x'
+    FURHAT_IP = '192.168.0.111'
     FURHAT_AGENT_NAME = 'furhat6'
     RABBITMQ_CONNECTION = {'host': 'localhost', 'port': 32777}
 
     def __init__(self, environment):
-        super(ArtificialAgent, self).__init__()
+        super(Agent, self).__init__()
         self.environment = environment
 
         # TODO: Move this to separate thread later... but for now listen for wizard events
-        self.listen_to_wizard_events()
+
+        self.thread = Thread(target = self.listen_to_wizard_events)
+        self.thread.deamon = True
+        self.thread.start()
 
 
     def listen_to_wizard_events(self):
-        connection = pika.BlockingConnection(pika.ConnectionParameters(**RABBITMQ_CONNECTION))
+        connection = pika.BlockingConnection(pika.ConnectionParameters(**self.RABBITMQ_CONNECTION))
         channel = connection.channel()
         channel.exchange_declare(exchange='wizard', type='topic')
         result = channel.queue_declare(exclusive=True)
@@ -29,11 +36,14 @@ class ArtificialAgent(Participant):
         # Callback for wizard events. map to furhat actions
         def callback(ch, method, properties, body):
             action = method.routing_key.rsplit('.', 1)[1]
+            msg = json.loads(body)
             if action == 'say':
-                self.say(body)
-            if action == 'gaze_at':
-                #self.environment.participant('A').angle_from_furhat
-                pass
+                self.say(msg['text'])
+            if action == 'accuse':
+                self.say('I accuse you')
+                location = self.environment.get_participants(msg['participant']).get_furhat_angle()
+                self.gaze_at({'x': 2, 'y': 0, 'z': 2})
+
 
         channel.basic_consume(callback, queue=queue_name)
         print(' [*] Waiting for messages. To exit press CTRL+C')
@@ -42,3 +52,9 @@ class ArtificialAgent(Participant):
     def say(self, text):
         with connect_to_iristk(self.FURHAT_IP) as furhat_client:
             furhat_client.say(self.FURHAT_AGENT_NAME, text)
+
+    def gaze_at(self, location):
+        with connect_to_iristk(self.FURHAT_IP) as furhat_client:
+            furhat_client.gaze(self.FURHAT_AGENT_NAME, location)
+
+a = Agent(None)
