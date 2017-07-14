c587eec3a2abe0842d9834c34ca5ae1abb3bf06b
Patrik Jonell <pjjonell@kth.se>
2017-07-14 11:21:23 +0100

    dialog acts in the wizard

--- src/main/python/wizard/static/js/main.js
+++ src/main/python/wizard/static/js/main.js
@@ -1,37 +1,87 @@
 $(document).ready(function() {
+    function reset() {
+        $('.list-group').removeClass('dim');
+    }
+
     var state = 0;
+    var combo = [];
+    var stop = false;
     $('html').on('keydown', function(event) {
+        if (stop) return;
         console.log(event.key)
         if ($(event.target).attr('id') === 'name') return true;
-        var keyPressed = event.key;
-        switch(keyPressed) {
-        default:
-            switch(keyPressed.toLowerCase()) {
-                case 'q':
-                  $.get('/say?text=hello');
-                  break;
-                case 'w':
-                  $.get('/say?text=good bye')
-                  break;
-                case 'a':
-                  $.get('/accuse?participant=1')
-                  break;
-                case 's':
-                  $.get('/accuse?participant=2')
-                  break;
-                case 'd':
-                  $.get('/accuse?participant=3')
-                  break;
-                case 'f':
-                  $.get('/accuse?participant=4')
-                  break;
-                case 'g':
-                  $.get('/accuse?participant=5')
-                  break;
-                case 'h':
-                  $.get('/accuse?participant=6')
-                  break;
+        var keyPressed = event.key.toLowerCase();
+        combo.push(keyPressed);
+        console.log(combo.sort().join('-'))
+
+        if (combo.length == 2) {
+            var modifier = null;
+            if (combo.indexOf('alt') !== -1){
+                modifier = 'support';
+            } else if (combo.indexOf('control') !== -1){
+                modifier = 'defend';
+            } else if (combo.indexOf('shift') !== -1){
+                modifier = 'accuse';
             }
+            if (modifier) {
+                var participant = combo.filter(item => item !== modifier)[0]
+                switch(participant) {
+                    case ' ':
+                      $.get(`/dialog_act?action=${modifier}&participant=general`)
+                      break;
+                    case '0':
+                      $.get(`/dialog_act?action=${modifier}&participant=self`)
+                      break;
+                    case '1':
+                      $.get(`/dialog_act?action=${modifier}&participant=black`)
+                      break;
+                    case '2':
+                      $.get(`/dialog_act?action=${modifier}&participant=brown`)
+                      break;
+                    case '3':
+                      $.get(`/dialog_act?action=${modifier}&participant=orange`)
+                      break;
+                    case '4':
+                      $.get(`/dialog_act?action=${modifier}&participant=blue`)
+                      break;
+                    case '5':
+                      $.get(`/dialog_act?action=${modifier}&participant=pink`)
+                      break;
+                    case '6':
+                      $.get(`/dialog_act?action=${modifier}&participant=white`)
+                      break;
+                }
+
+            }
+
         }
+
+
+        switch(combo.sort().join('-')) {
+            case 'q':
+              $.get('/say?text=yes');
+              break;
+            case 'w':
+              $.get('/say?text=no')
+              break;
+            case 'e':
+              $.get('/say?text=maybe')
+              break;
+            case 'shift':
+              $('.list-group:not(#accuse)').addClass('dim');
+              break;
+            case 'control':
+              $('.list-group:not(#defend)').addClass('dim');
+              break;
+            case 'alt':
+              $('.list-group:not(#support)').addClass('dim');
+              break;
+
+        }
+    });
+    $('html').on('keyup', function(event) {
+        combo = [];
+        stop = false;
+        reset();
     });
 });
