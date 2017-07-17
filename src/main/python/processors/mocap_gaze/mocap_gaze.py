a45c8e478c1379984c1671021c6869546e0acaf5
Dimosthenis Kontogiorgos <diko@kth.se>
2017-07-17 17:29:58 +0100

    Adds processor for mocap_gaze and son data.

--- src/main/python/processors/mocap_gaze/mocap_gaze.py
+++ src/main/python/processors/mocap_gaze/mocap_gaze.py
@@ -3,35 +3,25 @@ import pika
 import json
 import time
 import msgpack
+import re
+import sys
+sys.path.append('../..')
+from shared import MessageQueue
+import yaml
 
-# Iniciate la conneccion!
-credentials = pika.PlainCredentials('test', 'test')
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.0.108', credentials=credentials))
-channel = connection.channel()
-channel.exchange_declare(exchange='pre-processor', type='topic')
-
-result = channel.queue_declare(exclusive=True)
-queue_name = result.method.queue
-
-# Listen to mocap sensor
-channel.queue_bind(exchange='sensors', queue=queue_name, routing_key='mocap.new_sensor.*')
+# Settings
+SETTINGS_FILE = '../../settings.yaml'
+settings = yaml.safe_load(open(SETTINGS_FILE, 'r').read())
 
 # Procees input data
 def callback(ch, method, properties, body):
-    participant = method.routing_key.rsplit('.', 1)[1]
-    context = zmq.Context()
-    s = context.socket(zmq.SUB)
-    s.setsockopt_string(zmq.SUBSCRIBE, unicode(''))
-    s.connect(body)
+    #json.load
+    print(body)
+    print("-------------------------------------------------")
 
-    while True:
-        data = s.recv()
-        msgdata, timestamp = msgpack.unpackb(data, use_list=False)
-        print timestamp
-        #ch.basic_publish(exchange='pre-processor', routing_key='mocap.data.{}'.format(participant), body=json.dumps(data))
-    s.close()
+mq = MessageQueue()
 
-channel.basic_consume(callback, queue=queue_name)
+mq.bind_to_queue(exchange='pre-processor', routing_key="{}.*".format(settings['messaging']['mocap_processing']), callback=callback)
 
 print('[*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
+mq.listen()
