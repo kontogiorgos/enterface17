5b79a5aa91fd25fcdd5acf9af745e6121b836781
Patrik Jonell <pjjonell@kth.se>
2017-07-19 17:50:45 +0100

    added new bat scripts

---
+++ src/main/bat/pc2/run.bat
@@ -0,0 +1,8 @@
+REM start kinect sensor
+START CMD /K CALL "run_kinect_sensor.bat"
+
+REM start kinect screenreader hack
+START CMD /K CALL "../screenreader_hack.bat"
+
+REM start kinect python video sensor script
+START CMD /K CALL "start_screenrecording_video.bat"
