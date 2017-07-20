3a26ec1d119e2a6fbabf37b7b588ab64240a0235
Jose David Lopes <jdlopes@jdlopes-mbpro1.local>
2017-07-20 10:17:37 +0100

    Updated prompts and changed the agent_fatima.py to be compatible with the message queue

--- src/main/python/game/agent/agent_fatima.py
+++ src/main/python/game/agent/agent_fatima.py
@@ -7,6 +7,8 @@ import json
 import sys,os,re
 import random
 import csv
+sys.path.append('..')
+import Environment
 sys.path.append('../..')
 from shared import MessageQueue
 # etc..
@@ -32,7 +34,7 @@ class Agent(object):
 
 
     def listen_to_wizard_events(self):
-        mq = MessageQueue()
+        mq = MessageQueue('fatima_agent')
 
         #getting system prompts
         prompts_dict = {}
@@ -41,7 +43,6 @@ class Agent(object):
         for root,dir,files in os.walk(os.path.join(self.FURHAT_HOME,'NLG/wizard/')):
             for file in files:
                 if file.endswith('.prompts'):
-                    print(file)
                     with open(os.path.join(root,file),'r') as prompt_file:
                         if not file.split('.prompts')[0] in prompts_dict:
                             prompts_dict[file.split('.prompts')[0]] = []
@@ -80,28 +81,26 @@ class Agent(object):
                 selected_prompt = random.choice(prompts_dict['summary'])
             return selected_prompt
 
-
         # Callback for wizard events. map to furhat actions
-        def callback(ch, method, properties, body):
-            action = method.routing_key.rsplit('.', 1)[1]
-            msg = json.loads(body)
+        def callback(_mq, get_shifted_time, routing_key, body):
+            action = routing_key.rsplit('.', 1)[1]
+            msg = body
             if action == 'say':
                 self.say(msg['text'])
             if action == 'accuse':
                 if get_prompt(action,prompts_dict,msg['participant']):
-                    #spoken_prompt_list.append(get_prompt(action,prompts_dict,msg['participant']))
                     self.say(get_prompt(action,prompts_dict,msg['participant']))
-                    #location = self.environment.get_participants(msg['participant']).get_furhat_angle()
-                    #self.gaze_at(location)
+                    location = self.environment.get_participants(msg['participant']).get_furhat_angle()
+                    self.gaze_at(location)
             if action == 'defend':
                 if get_prompt(action,prompts_dict,msg['participant']):
                     spoken_prompt_list.append(get_prompt(action,prompts_dict,msg['participant']))
                     self.say(get_prompt(action,prompts_dict,msg['participant']))
-                    #if random.choice(['last_speaker','defendee']) == 'last_speaker':
-                    #    self.gaze_at({'x':0,'y':0,'z':0})
-                    #else:
-                    #    location = self.environment.get_participants(msg['participant']).get_furhat_angle()
-                    #    self.gaze_at(location)
+                    if random.choice(['last_speaker','defendee']) == 'last_speaker':
+                        self.gaze_at({'x':0,'y':0,'z':0})
+                    else:
+                        location = self.environment.get_participants(msg['participant']).get_furhat_angle()
+                        self.gaze_at(location)
 
             if action == 'support':
                 if get_prompt(action,prompts_dict,msg['participant']):
@@ -109,32 +108,32 @@ class Agent(object):
                     self.say(get_prompt(action,prompts_dict,msg['participant']))
                 if msg['participant'] == 'general':
                     self.gaze_at({'x':0,'y':0,'z':0})
-                #else:
-                #    location = self.environment.get_participants(msg['participant']).get_furhat_angle()
-                #    self.gaze_at(location)
+                else:
+                    location = self.environment.get_participants(msg['participant']).get_furhat_angle()
+                    self.gaze_at(location)
 
             if action == 'vote':
                 if get_prompt(action,prompts_dict,msg['participant']):
                     spoken_prompt_list.append(get_prompt(action,prompts_dict,msg['participant']))
                     self.say(get_prompt(action,prompts_dict,msg['participant']))
-                    #location = self.environment.get_participants(msg['participant']).get_furhat_angle()
-                    #self.gaze_at(location)
+                    location = self.environment.get_participants(msg['participant']).get_furhat_angle()
+                    self.gaze_at(location)
 
             if action == 'small_talk':
                 if get_prompt(action,prompts_dict,msg['participant']):
                     spoken_prompt_list.append(get_prompt(action,prompts_dict,msg['participant']))
                     self.say(get_prompt(action,prompts_dict,msg['participant']))
-                 #   if msg['participant']:
-                 #       location = self.environment.get_participants(msg['participant']).get_furhat_angle()
-                 #       self.gaze_at(location)
-                 #   else:
-                 #       self.gaze_at({'x':0,'y':0,'z':0})
+                    if msg['participant']:
+                        location = self.environment.get_participants(msg['participant']).get_furhat_angle()
+                        self.gaze_at(location)
+                    else:
+                        self.gaze_at({'x':0,'y':0,'z':0})
 
             if action == 'summary':
                 self.say(get_summary())
                 spoken_prompt_list.append(get_summary())
 
-        mq.bind_to_queue(
+        mq.bind_queue(
             exchange='wizard', routing_key='action.*', callback=callback
         )
 
@@ -150,4 +149,5 @@ class Agent(object):
         with connect_to_iristk(self.FURHAT_IP) as furhat_client:
             furhat_client.gaze(self.FURHAT_AGENT_NAME, location)
 
-a = Agent(None)
+env = Environment.Environment()
+a = Agent(env)
