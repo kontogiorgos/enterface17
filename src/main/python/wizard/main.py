21b2bf9feee4f4c87202f897f4eb25442ed5ed64
Per Fallgren <perfall@perfall-mbpro.local>
2017-07-20 16:16:52 +0200

    Updated FAtiMA.py

--- src/main/python/wizard/main.py
+++ src/main/python/wizard/main.py
@@ -47,3 +47,53 @@ def visualizations():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
+
+    from threading import Thread
+
+    def run():
+        print('I am running in my own thread!')
+
+        fatima_listner_mq = MessageQueue('fatima_listener')
+
+        def update_belief_interface(msg):
+            participant = msg['participant']
+            belief = msg['belief']
+            print(participant,belief)
+            #send_to_wizard(participant,belief)
+
+        def display_suggested_action(msg):
+            print(msg['action'])
+            #send_to_wizard(msg['action'])
+
+        def display_suggested_vote(msg):
+            print(msg['participant'])
+            #send_to_wizard(msg['participant'])
+
+        def update_wizard(_mq, get_shifted_time, routing_key, body):
+
+            action = routing_key
+            msg = body
+
+            if action == 'belief_update':
+                update_belief_interface(msg)
+
+            if action == 'suggest_action':
+                display_suggested_action(msg)
+
+            if action == 'suggest_vote':
+                display_suggested_vote(msg)
+
+
+        fatima_listner_mq.bind_queue(
+            exchange='fatima_agent', routing_key='*', callback=update_wizard
+        )
+
+        print('[*] Waiting for messages. To exit press CTRL+C')
+        fatima_listner_mq.listen()
+
+
+    thread = Thread(target=run)
+    thread.deamon = True
+    thread.start()
+    while True:
+        pass
