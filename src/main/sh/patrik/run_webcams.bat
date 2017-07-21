2027810e14d15f3580d2362bb3b5bbba38f74040
Patrik Jonell <pjjonell@kth.se>
2017-07-21 13:09:05 +0100

    add bash scripts

--- src/main/sh/patrik/run_webcams.bat
+++
@@ -1,8 +0,0 @@
-REM start python webcam sensor blue
-START CMD /K CALL "run_video-blue.bat"
-
-REM start python webcam sensor brown
-START CMD /K CALL "run_video-brown.bat"
-
-REM start python webcam sensor white
-START CMD /K CALL "run_video-white.bat"
