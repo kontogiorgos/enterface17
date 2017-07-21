f3f98ddfcf87ed1a4f3d3b24cba1b4814f0883cd
Dimosthenis Kontogiorgos <diko@kth.se>
2017-07-21 14:14:49 +0100

    Updates mocap bat scripts.

--- src/main/bat/mocap/run.bat
+++ src/main/bat/mocap/run.bat
@@ -1,8 +1,35 @@
 REM start mocap sensor
 REM START CMD /K CALL "mocap.bat"
 
+REM start python webcam sensors
+REM CALL "run_webcams.bat"
+
 REM start mocap preprocessor
 START CMD /K CALL "mocap_preprocessor.bat"
 
-REM start python webcam sensors
-CALL "run_webcams.bat"
+REM start asr preprocessor
+START CMD /K CALL "run_asr.bat"
+
+REM start asr logger
+START CMD /K CALL "run_asrlogger.bat"
+
+REM start disk logger
+START CMD /K CALL "run_disklogger.bat"
+
+REM start facial features processor
+START CMD /K CALL "run_facialfeatures.bat"
+
+REM start position processor
+START CMD /K CALL "run_position.bat"
+
+REM start timeserver
+START CMD /K CALL "run_timeserver.bat"
+
+REM start tobii blue sensor
+REM START CMD /K CALL "run_tobii-blue.bat"
+
+REM start tobii brown sensor
+START CMD /K CALL "run_tobii-brown.bat"
+
+REM start tobii white sensor
+REM START CMD /K CALL "run_tobii-white.bat"
