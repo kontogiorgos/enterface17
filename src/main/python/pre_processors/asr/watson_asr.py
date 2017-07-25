08199559957907f8e48752e22d17e3fd659ded8a
Patrik Jonell <pjjonell@kth.se>
2017-07-25 01:08:05 +0100

    put ASR stuff into a class and dealt with some error handling. hopefully will work now

--- src/main/python/pre_processors/asr/watson_asr.py
+++ src/main/python/pre_processors/asr/watson_asr.py
@@ -14,6 +14,9 @@ sys.path.append('../..')
 from shared import MessageQueue
 import urllib.parse
 
+
+DEBUG = False
+
 with open('.watson_credentials.json') as f:
     credentials = json.loads(f.read())
 if not credentials:
@@ -26,93 +29,126 @@ token = authorization.get_token(url=api_base_url)
 def create_regognition_method_str(api_base_url):
 	parsed_url = urllib.parse.urlparse(api_base_url)
 	return urllib.parse.urlunparse(("wss", parsed_url.netloc, parsed_url.path + "/v1/recognize", parsed_url.params, parsed_url.query, parsed_url.fragment, ))
-	
+
 recognition_method_url = create_regognition_method_str(api_base_url)
 
-timer = None
-last_timer = None
+
+
+class WatsonASR(object):
+    START_MESSAGE = {
+      'action': 'start',
+      'content-type': 'audio/l16;rate=44100',
+      'word_confidence': True,
+      'timestamps': True,
+      'continuous' : True,
+      'interim_results' : True,
+      'inactivity_timeout ': -1
+    }
+
+    def __init__(self, zmq_address, api_base_url, token, on_message_callback):
+        self.zmq_address = zmq_address
+        self.api_base_url = api_base_url
+        self.token = token
+        self.on_message_callback = on_message_callback
+
+        self._running = True
+        self.timer = None
+        self.last_timer = None
+
+        thread = Thread(target = self.connect_to_watson)
+        thread.deamon = True
+        thread.start()
+
+
+    def run(self, ws):
+        context = zmq.Context()
+        s = context.socket(zmq.SUB)
+        s.setsockopt_string( zmq.SUBSCRIBE, u'')
+        s.connect(self.zmq_address)
+
+
+        if DEBUG: print(json.dumps(self.START_MESSAGE))
+        ws.send(json.dumps(self.START_MESSAGE).encode('utf-8'))
+
+        while True:
+            data = s.recv()
+            if data == b'CLOSE':
+                print("got CLOSE msg. stopping..")
+                self._running = False
+                break
+            msgdata, timestamp = msgpack.unpackb(data, use_list=False)
+            if not self.timer: self.timer = timestamp
+            self.last_timer = timestamp
+            try:
+                ws.send(msgdata, websocket.ABNF.OPCODE_BINARY)
+            except websocket._exceptions.WebSocketConnectionClosedException:
+                print("couldn't send, restarting...")
+                break
+        s.close()
+        ws.close()
+        print("thread terminating...")
+
+    def on_open(self, ws):
+        thread = Thread(target = self.run, args=(ws, ))
+        thread.deamon = True
+        thread.start()
+
+    def on_error(self, ws, error):
+        """Print any errors."""
+        print('ERROR', error)
+
+    def on_close(self, ws):
+        print("Closed down websocket")
+
+    def connect_to_watson(self):
+        headers = {'X-Watson-Authorization-Token': self.token}
+
+        while self._running:
+            try:
+                if DEBUG: print('connecting to watson...')
+                ws = websocket.WebSocketApp(
+                    self.api_base_url,
+                    header=headers,
+                    on_message=self.on_message,
+                    on_error=self.on_error,
+                    on_close=self.on_close,
+                    on_open=self.on_open
+                )
+                ws.run_forever()
+            except:
+                'restarting'
+        print('DONE')
+
+    def on_message(self, ws, m):
+        msg = json.loads(str(m))
+        if msg.get('error'):
+            print('>> {}'.format(msg))
+
+        if msg.get('results'):
+            data = {
+                'time_start_asr': self.timer,
+                'time_until_asr': self.last_timer,
+                'text': msg['results'][0].get('alternatives', [{}])[0].get('transcript'),
+                'final': msg["results"][0]["final"],
+                'confidence': msg['results'][0].get('alternatives', [{}])[0].get('confidence')
+            }
+            if msg["results"][0]["final"]:
+                self.timer = None
+            self.on_message_callback(data)
+
 
 
 def callback(_mq, get_shifted_time, routing_key, body):
     participant = routing_key.rsplit('.', 1)[1]
-
     print('connected {}'.format(routing_key))
 
+    def on_message(data):
+        if DEBUG: print(data)
+        routing_key = 'asr.data' if data["final"] else 'asr.incremental_data'
+        _mq.publish(exchange='pre-processor', routing_key='{}.{}'.format(routing_key,participant), body=data)
 
-    def connect_to_watson():
-        def on_open(ws):
-            def run():
-                global timer, last_timer
-                context = zmq.Context()
-                s = context.socket(zmq.SUB)
-                s.setsockopt_string( zmq.SUBSCRIBE, '' )
-                s.connect(body.get('address'))
-                message = {
-                  'action': 'start',
-                  'content-type': 'audio/l16;rate=44100',
-                  'word_confidence': True,
-                  'timestamps': True,
-                  'continuous' : True,
-                  'interim_results' : True,
-                }
+    WatsonASR(body.get('address'), recognition_method_url, token, on_message)
 
-                print(json.dumps(message))
-                ws.send(json.dumps(message).encode('utf-8'))
-
-                while True:
-                    data = s.recv()
-                    msgdata, timestamp = msgpack.unpackb(data, use_list=False)
-                    if not timer: timer = timestamp
-                    last_timer = timestamp
-                    ws.send(msgdata, websocket.ABNF.OPCODE_BINARY)
-                s.close()
-                ws.close()
-                print("thread terminating...")
-            thread = Thread(target = run)
-            thread.deamon = True
-            thread.start()
-
-        def on_error(self, error):
-            """Print any errors."""
-            print(error)
-
-        def on_close(ws):
-            print("Closed down")
-
-        def on_message(ws, m):
-            global timer
-            msg = json.loads(str(m))
-            if msg.get('results'):
-                data = {
-                    'time_start_asr': timer,
-                    'time_until_asr': last_timer,
-                    'text': msg['results'][0].get('alternatives', [{}])[0].get('transcript')
-                }
-                timer = None
-
-                routing_key = 'asr.data.{}' if msg["results"][0]["final"] else 'asr.incremental_data.{}'
-                _mq.publish(exchange='pre-processor', routing_key='{}.data.{}'.format(routing_key,participant), body=data)
-
-
-        headers = {'X-Watson-Authorization-Token': token}
-
-        while True:
-            try:
-            	# 'wss://stream.watsonplatform.net/speech-to-text/api/v1/recognize'
-                ws = websocket.WebSocketApp(recognition_method_url,
-                                            header=headers,
-                                            on_message=on_message,
-                                            on_error=on_error,
-                                            on_close=on_close)
-                ws.on_open = on_open
-                ws.run_forever()
-            except:
-                'restarting'
-
-
-    thread = Thread(target = connect_to_watson)
-    thread.deamon = True
-    thread.start()
 
 mq = MessageQueue('watson-asr-preprocessor')
 mq.bind_queue(
